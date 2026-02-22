c71d4bd26b9e807ec566d337e7f14cfee892659b
kndevelop <kn02sb04@gmail.com>
2026-02-22 13:25:42 +0900

    参照側にも見えるようにimplementation→apiに変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,10 @@ repositories {
 }
 
 dependencies {
+	api("org.springframework.boot:spring-boot-starter-security")
+	api("org.springframework.boot:spring-boot-starter-validation")
+	api("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
